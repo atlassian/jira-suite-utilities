dd72b1df709fec22af068a410a8ab7950ec8b511
Sergio Cinos <scinos@atlassian.com>
2013-04-02 14:58:26 +1100

    JRA-32294 - Make the loading of Google Maps more robust. Check the 'reason' parameter from JIRA.Events.NEW_CONTENT_ADDED before looking for maps

--- src/main/resources/com/googlecode/jsu/maps/LocationField.js
+++ src/main/resources/com/googlecode/jsu/maps/LocationField.js
@@ -1,17 +1,65 @@
 (function($) {
 
-    function ensureGoogleMapsIsLoaded(callback) {
-        if (typeof google !== 'undefined' && typeof google.maps !== "undefined") {
-            callback();
-        } else {
-            window.onGMapsLoad = callback;
-            $("body").append($("<script src='https://maps-api-ssl.google.com/maps/api/js?v=3&sensor=false&callback=onGMapsLoad'></script>"));
+    var global = window,
+        JSONP_CALLBACK_NAME = "onJSUGMapsLoad",
+        mapsCallbacks = [],
+        googleLoaderRequested = false,
+        googleMapsRequested = false;
+
+    function runMapsCallbacks() {
+        var cb;
+        while (cb = mapsCallbacks.shift()) {
+            cb();
         }
     }
 
+    function loadMapAndRunCallbacks(callback) {
+        var isGoogleMapsPresent = global.google.maps && global.google.maps.LatLng;
+
+        if (isGoogleMapsPresent) {
+            //Google Maps is here, run all the pending callbacks
+            mapsCallbacks.push(callback);
+            runMapsCallbacks();
+
+        } else if (googleMapsRequested) {
+            //Google Maps is not present but has been requested -> Add the callback to the maps queue,
+            //it will be eventually executed by loadMapAndRunCallbacks()
+            mapsCallbacks.push(callback);
+
+        } else {
+            //Google Maps is not present nor being requested -> Add the callback to the queue and request it
+            googleMapsRequested = true;
+            mapsCallbacks.push(callback);
+            google.load("maps","3", {other_params:'sensor=false', callback: runMapsCallbacks} );
+        }
+    }
+
+    function runWithGoogleMaps(callback) {
+        if (global.google && global.google.load) {
+            //Google Loader is present -> Load GMaps and run the callback
+            loadMapAndRunCallbacks(callback);
+
+        } else if (googleLoaderRequested) {
+            //Google Loader is not present but has been requested -> Add the callback to the maps queue,
+            //it will be eventually executed by loadMapAndRunCallbacks()
+            mapsCallbacks.push(callback);
+
+        } else {
+            // Google Loader is not present nor requested -> Request it and add the callback to the maps queue
+            googleLoaderRequested = true;
+            global[JSONP_CALLBACK_NAME] = function() {
+                loadMapAndRunCallbacks(callback);
+            };
+            var script = document.createElement("script");
+            script.src = "https://www.google.com/jsapi?callback="+JSONP_CALLBACK_NAME;
+            script.type = "text/javascript";
+            $("body").append(script);
+        }
+    }
+
+
     function loadAndDisplayLocationOnMap(location, callback) {
-        ensureGoogleMapsIsLoaded(function() {
-            window.onGMapsLoad = null;
+        runWithGoogleMaps(function() {
             new google.maps.Geocoder().geocode({
                 address: location
             }, callback);
@@ -55,16 +103,18 @@
         });
     }
 
-    JIRA.bind(JIRA.Events.NEW_CONTENT_ADDED, function (e, context) {
-        var jsuMapFields = $(".jsuLocation.ready", context);
-        jsuMapFields.removeClass("ready");
-        jsuMapFields.each(function(i, jsuMapField) {
-            jsuMapField = $(jsuMapField);
-            jsuMapField.find("> a").click(function(e) {
-                e.preventDefault();
-                toggleMap(jsuMapField);
-            })
-        });
+    JIRA.bind(JIRA.Events.NEW_CONTENT_ADDED, function (e, context, reason) {
+        if (reason === JIRA.CONTENT_ADDED_REASON.panelRefreshed || reason === JIRA.CONTENT_ADDED_REASON.pageLoad) {
+            var jsuMapFields = $(".jsuLocation.ready", context);
+            jsuMapFields.removeClass("ready");
+            jsuMapFields.each(function(i, jsuMapField) {
+                jsuMapField = $(jsuMapField);
+                jsuMapField.find("> a").click(function(e) {
+                    e.preventDefault();
+                    toggleMap(jsuMapField);
+                })
+            });
+        }
     });
 
     JIRA.bind(JIRA.Events.INLINE_EDIT_STARTED, function() {
