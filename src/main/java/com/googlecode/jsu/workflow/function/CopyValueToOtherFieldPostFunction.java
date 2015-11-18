066cc52791dfc5b814e2ec2899a535254c177d31
rpopov <rpopov@ifao.net>
2015-11-18 02:42:29 +0200

    added CopyValueToOtherFieldPostFunction post-function

---
+++ src/main/java/com/googlecode/jsu/workflow/function/CopyValueToOtherFieldPostFunction.java
@@ -0,0 +1,103 @@
+package com.googlecode.jsu.workflow.function;
+
+
+import static com.googlecode.jsu.workflow.WorkflowCopyValueFromOtherFieldPostFunctionPluginFactory.PARAM_SOURCE_FIELD;
+import static com.googlecode.jsu.workflow.WorkflowCopyValueFromOtherFieldPostFunctionPluginFactory.PARAM_DEST_FIELD;
+import static com.googlecode.jsu.workflow.WorkflowCopyValueFromOtherFieldPostFunctionPluginFactory.PARAM_COPY_TYPE;
+
+import java.util.Map;
+
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.issue.MutableIssue;
+import com.atlassian.jira.issue.fields.Field;
+import com.atlassian.jira.issue.util.IssueChangeHolder;
+import com.atlassian.jira.user.ApplicationUser;
+import com.atlassian.jira.util.I18nHelper;
+import com.googlecode.jsu.util.WorkflowUtils;
+import com.opensymphony.module.propertyset.PropertySet;
+import com.opensymphony.workflow.WorkflowException;
+
+/**
+ * @author Gustavo Martin
+ * Adapted from  CopyValueFromOtherFieldPostFunction by R.Popov to update the *to* issue (this or parent)
+ *
+ * This function copies the value to a field to another one.
+ */
+public class CopyValueToOtherFieldPostFunction extends AbstractPreserveChangesPostFunction {
+    private final WorkflowUtils workflowUtils;
+    private final I18nHelper.BeanFactory beanFactory;
+
+    public CopyValueToOtherFieldPostFunction(WorkflowUtils workflowUtils, I18nHelper.BeanFactory beanFactory) {
+        this.workflowUtils = workflowUtils;
+        this.beanFactory = beanFactory;
+    }
+
+    /* (non-Javadoc)
+     * @see com.googlecode.jsu.workflow.function.AbstractPreserveChangesPostFunction#executeFunction(java.util.Map, java.util.Map, com.opensymphony.module.propertyset.PropertySet, com.atlassian.jira.issue.util.IssueChangeHolder)
+     */
+    @Override
+    protected void executeFunction(
+            Map<String, Object> transientVars, Map<String, String> args,
+            PropertySet ps, IssueChangeHolder holder
+    ) throws WorkflowException {
+        String fieldFromKey = args.get(PARAM_SOURCE_FIELD);
+        String fieldToKey = args.get(PARAM_DEST_FIELD);
+        String copyType = args.get(PARAM_COPY_TYPE);
+
+        Field fieldFrom = workflowUtils.getFieldFromKey(fieldFromKey);
+        Field fieldTo = workflowUtils.getFieldFromKey(fieldToKey);
+
+        String fieldFromName = (fieldFrom != null) ? fieldFrom.getName() : fieldFromKey;
+        String fieldToName = (fieldTo != null) ? fieldTo.getName() : fieldToKey;
+
+        try {
+            ApplicationUser currentUser = getCallerUser(transientVars, args);
+            MutableIssue issue = getIssue(transientVars);
+            MutableIssue sourceIssue;
+            MutableIssue destIssue;
+
+            if("parent".equals(copyType)) {
+                sourceIssue = issue;
+                destIssue = (MutableIssue) issue.getParentObject();
+
+                if(destIssue==null) {
+                    log.debug("Issue (" + sourceIssue.getKey() + ") has no parent, wont do anything.");
+                    return;
+                }
+            } else {
+                //either same, not set, past versions of this did not have this feature, they will be unset
+                sourceIssue = issue;
+                destIssue = issue;
+            }
+
+            // It gives the value from the source field.
+            Object sourceValue = workflowUtils.getFieldValueFromIssue(sourceIssue, fieldFrom, true);
+
+            if (log.isDebugEnabled()) {
+                log.debug(
+                        String.format(
+                                "Copying value [%s] from issue %s field '%s' to issue %s field '%s'",
+                                sourceValue,
+                                sourceIssue.getKey(),
+                                fieldFromName,
+                                destIssue.getKey(),
+                                fieldToName
+                        )
+                );
+            }
+
+            // It set the value to field.
+            workflowUtils.setFieldValue(currentUser, destIssue, fieldToKey, sourceValue, holder);
+
+            if (log.isDebugEnabled()) {
+                log.debug("Value was successfully copied");
+            }
+        } catch (Exception e) {
+            I18nHelper i18nh = this.beanFactory.getInstance(
+                    ComponentAccessor.getJiraAuthenticationContext().getUser().getDirectoryUser());
+            String message = i18nh.getText("copyvaluefromfield-function-view.unable_to_copy",fieldFromName,fieldToName);
+            log.error(message, e);
+            throw new WorkflowException(message);
+        }
+    }
+}
